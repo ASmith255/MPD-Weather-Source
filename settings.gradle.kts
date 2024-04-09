ccddbc3cc3e46f271a2b7e7904b15cbc4070bebe
ASmith255 <asmith255@caledonian.ac.uk>
2024-04-09 22:16:31 +0100

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Smith_Alexander_S2028248"
+include(":app")
